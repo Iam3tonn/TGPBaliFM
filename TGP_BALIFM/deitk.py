d106c3cdeecbf1982aa77d6d0644092347464e73
Dmitriy Krasnyh <149979045+Iam3tonn@users.noreply.github.com>
2023-11-26 03:16:29 +0300

    Add files via upload

--- TGP_BALIFM/deitk.py
+++ TGP_BALIFM/deitk.py
@@ -9,10 +9,19 @@ def run():
         if response.status_code == 200:
             article_soup = BeautifulSoup(response.text, 'html.parser')
             article_text_element = article_soup.find('div', class_='detail__body-text')
-            return article_text_element.text.strip() if article_text_element else None
+            date_element = article_soup.find('div', class_='detail__date')  # New line to extract date
+
+            return article_text_element.text if article_text_element else None, date_element.text if date_element else None
         else:
             print(f'Error fetching article content from {article_url}')
-            return None
+            return None, None
+
+    def process_date(date_str):
+        # Remove the first word before the comma and the word "WIB"
+        date_parts = date_str.split(',')
+        if len(date_parts) > 1:
+            return ','.join(date_parts[1:]).replace('WIB', '').strip()
+        return date_str
 
     url = 'https://www.detik.com/bali/berita'
     response = requests.get(url)
@@ -25,7 +34,7 @@ def run():
         for article in soup.find_all('article'):
             title_element = article.find('h2')
             if title_element:
-                title = title_element.text.strip()
+                title = title_element.text
                 link = article.find('a')['href']
 
                 # Check if the title is one of the specified titles
@@ -33,17 +42,13 @@ def run():
                 if title in specified_titles:
                     detail_title_element = article.find('div', class_='h1.detail__title')
                     if detail_title_element:
-                        title = detail_title_element.text.strip()
-
-                # Extract the article date
-                detail_date_element = article.find('div', class_='detail__date')
-                article_date = detail_date_element.text.strip() if detail_date_element else None
+                        title = detail_title_element.text
 
                 # Translate the title from Indonesian to Russian
                 translated_title = translator.translate(title, src='id', dest='ru').text
 
-                # Get the full text from the article page
-                article_text = get_article_text(link)
+                # Get the full text and date from the article page
+                article_text, article_date = get_article_text(link)
 
                 # Remove "\nSimak Video" and specified advertisement content
                 if article_text:
@@ -52,7 +57,10 @@ def run():
                     # Remove any combination of \r, \t, \n from the description
                     article_text = article_text.replace("\r", "").replace("\t", "").replace("\n", "")
 
-                news_items.append({'title': translated_title, 'link': link, 'date': article_date, 'description': article_text})
+                # Process and format the date
+                formatted_date = process_date(article_date)
+
+                news_items.append({'title': translated_title, 'link': link, 'description': article_text, 'date': formatted_date})
 
         with open('1) Json folder/detik.json', 'w', encoding='utf-8') as json_file:
             json.dump(news_items, json_file, ensure_ascii=False, indent=4)
